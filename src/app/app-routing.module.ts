54afe6e872f3d8bb6a77c99710df06b7d4c93161
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-09-23 15:39:55 +0530

    about cmponent done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
-  {path:"profile",component:ProfileComponent}
+  {path:"profile",component:ProfileComponent},
+  {path:"about",component:AboutComponent}
 ];
 
 @NgModule({
