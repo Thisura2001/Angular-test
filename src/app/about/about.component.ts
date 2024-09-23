68916c8779e448e36d258e645664eb7b880651d9
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-09-23 21:08:43 +0530

    create array to append data to the table

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-
+export interface student{
+  id:string,
+  name:string,
+  address:string,
+  email:string
+}
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  showAlert() {
-    alert('Button clicked!');
-    console.log("alert send");
-    
-  }
+  students: student[]=[
+    {id:'1',name:'kamal',address:'matugama',email:'kamal@gmail.com'},
+    {id:'2',name:'nimal',address:'jaffna',email:'nimal@gmail.com'},
+    {id:'3',name:'bimal',address:'kalurtara',email:'bimal@gmail.com'},
+    {id:'4',name:'saman',address:'kalutara',email:'saman@gmail.com'}
+  ];
 }
